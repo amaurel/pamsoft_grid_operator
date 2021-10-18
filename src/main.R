4cabe71721b20e713639acab311cbfe567fc2112
Alexandre Maurel <alexandre.maurel@gmail.com>
2021-10-18 17:32:45 +0200

    remove src

--- src/main.R
+++
@@ -1,9 +0,0 @@
-library(tercen)
-library(dplyr)
-
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(mean = mean(.y)) %>%
-  ctx$addNamespace() %>%
-  ctx$save()
